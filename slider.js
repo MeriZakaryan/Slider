bcfbb20a6441d373898a079c0014aedb9fc5f163
MeriZakaryan <merizakaryan2005@gmail.com>
2021-05-01 14:48:32 +0400

    changes

---
+++ slider.js
@@ -0,0 +1,10 @@
+let counter = 1
+setInterval(()=>{
+counter===4 ? counter=1: document.querySelector(".img").style.backgroundImage=`url(./img_${++counter}.jpg)`
+}, 2000)
+const prevImage = () =>{
+    counter===1? counter = 4 :  document.querySelector(".img").style.backgroundImage=`url(./img_${--counter}.jpg)`   
+}
+const nextImage = () =>{
+    counter===4? counter = 1 :  document.querySelector(".img").style.backgroundImage=`url(./img_${++counter}.jpg)`   
+}
