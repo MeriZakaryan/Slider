b0cc060ca1919b470519876f0591af514b9497c2
MeriZakaryan <merizakaryan2005@gmail.com>
2021-05-06 21:54:35 +0400

    changes

--- slider.js
+++ slider.js
@@ -1,10 +1,16 @@
-let counter = 1
-setInterval(()=>{
-counter===4 ? counter=1: document.querySelector(".img").style.backgroundImage=`url(./img_${++counter}.jpg)`
-}, 2000)
-const prevImage = () =>{
-    counter===1? counter = 4 :  document.querySelector(".img").style.backgroundImage=`url(./img_${--counter}.jpg)`   
+let counter = 0
+let colaction = {
+    img_1: "./images/img_1.jpg",
+    img_2: "./images/img_2.jpg",
+    img_3: "./images/img_3.jpg",
+    img_4: "./images/img_4.jpg",
 }
-const nextImage = () =>{
-    counter===4? counter = 1 :  document.querySelector(".img").style.backgroundImage=`url(./img_${++counter}.jpg)`   
+setInterval(() => {
+    counter === 4 ? counter = 1 : document.querySelector(".img").style.backgroundImage = `url(${colaction[`img_${++counter}`]})`
+}, 2000)
+const prevImage = () => {
+    counter === 1 ? counter = 4 : document.querySelector(".img").style.backgroundImage = `url(${colaction[`img_${--counter}`]})`
+}
+const nextImage = () => {
+    counter === 4 ? counter = 1 : document.querySelector(".img").style.backgroundImage = `url(${colaction[`img_${++counter}`]})`
 }
